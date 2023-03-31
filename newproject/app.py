1fcccf963bf080ba4be5a5c6196677ef9f48d9fa
Keegan2021 <Keegan.cox@go.mnstate.edu>
2023-03-31 17:23:58 -0500

    added initial files

---
+++ newproject/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/app1')
+def app1():
+    return render_template('app1.html')
+
+@app.route('/app2')
+def app2():
+    return render_template('app2.html')
+
+@app.route('/colors')
+def colors():
+    return render_template('colors.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
